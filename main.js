46d82c5823f5e363c5f6e11a2e848ca2784fa433
YazeedSal <yazeedsalameh02082005@gmail.com>
2022-01-20 16:19:57 +0200

    made a navbar aboce and put a sidenav that opens on mobile and it's ready as a first model

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+// theSideNavScr
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+
